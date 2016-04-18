4a7234ce86ca2f6decb576542f7d1b92bd83438b
JeanValjean2 <github@acemod.fr>
2016-04-18 12:33:34 +0200

    Stabilisation
    Correction du plusieurs petits bugs issus des nouvelles versions d'Xcode et stabilisation du script : il toussait très fort avec certaines archives...

--- importIPA.sh
+++ importIPA.sh
@@ -10,14 +10,14 @@
 #http://help.apple.com/deployment/ios/#/apda0e3426d7
 #http://blog.octo.com/en/automating-over-the-air-deployment-for-iphone/
 
+[[ `type -P xml2` ]] || echo "Le programme xml2 n'est pas disponible.";
+
 [[ $# -lt 3 ]] && echo "Ce script attend 3 paramètres : le chemin vers un fichier IPA, le dossier local dans lequel déposer le produit déployable et l'URL correspondant à la racine du site de déploiement." && exit 1;
 
 rm -rf tmp/
 mkdir tmp
 IPA=$1
 IPAFileName=`basename "$IPA"`
-IPASubDirName=`basename "$IPA" .ipa | tr A-Z a-z`
-IPASubDirName=`basename "$IPA" .ipa`
 RootDir=`basename "$2"`
 
 #2015-12-30 : on prend en compte n'importe quelle URL de dépôt en nettoyant au mieux le paramètre passé sur la CLI
@@ -32,30 +32,31 @@ RootURL="https://$T"
 
 echo "Traitement de $IPA..."
 sleep 1
-unzip -q "$IPA" -d tmp/ "Payload/$IPASubDirName.app/Info.plist" "Payload/$IPASubDirName.app/embedded.mobileprovision"
+unzip -DD -qq "$IPA" -d tmp/ 
+IPASubDirName=$(basename `find tmp/Payload/ -maxdepth 1 | tail -n1`)
+echo "Sous-dossier : "$IPASubDirName
 sleep 1
 
 #2015-09-21 : Xcode 7 envoie ses plist en texte et non plus en .plist binaire.
-file "tmp/Payload/$IPASubDirName.app/Info.plist" | grep -i text > /dev/null
+file "tmp/Payload/$IPASubDirName/Info.plist" | grep -i text > /dev/null
 IsText=$?
 
 #On commence par parser le fichier Info.plist qui contient certaines informations sur l'IPA qui vont permettre de construire à 90% les fichiers permettant d'installer l'application
 ParseInfo="plistutil -i"
 [[ $IsText == 0 ]] && ParseInfo="cat";
-BUNDLE_IDENTIFIER=`$ParseInfo "tmp/Payload/$IPASubDirName.app/Info.plist" | xml2 | grep -A 1 CFBundleIdentifier | tail -n1 | cut -d '=' -f 2`
+BUNDLE_IDENTIFIER=`$ParseInfo "tmp/Payload/$IPASubDirName/Info.plist" | xml2 | grep -A 1 CFBundleIdentifier | tail -n1 | cut -d '=' -f 2`
 #echo $BUNDLE_IDENTIFIER
-BUNDLE_VERSION=`$ParseInfo "tmp/Payload/$IPASubDirName.app/Info.plist" | xml2 | grep -A 1 CFBundleVersion | tail -n1 | cut -d '=' -f 2`
-BUNDLE_SHORTVERSION=`$ParseInfo "tmp/Payload/$IPASubDirName.app/Info.plist" | xml2 | grep -A 1 CFBundleShortVersionString | tail -n1 | cut -d '=' -f 2`
-BUNDLE_TITLE=`$ParseInfo "tmp/Payload/$IPASubDirName.app/Info.plist" | xml2 | grep -A 1 CFBundleDisplayName | tail -n1 | cut -d '=' -f 2`
+BUNDLE_VERSION=`$ParseInfo "tmp/Payload/$IPASubDirName/Info.plist" | xml2 | grep -A 1 CFBundleVersion | tail -n1 | cut -d '=' -f 2`
+BUNDLE_SHORTVERSION=`$ParseInfo "tmp/Payload/$IPASubDirName/Info.plist" | xml2 | grep -A 1 CFBundleShortVersionString | tail -n1 | cut -d '=' -f 2`
+BUNDLE_TITLE=`$ParseInfo "tmp/Payload/$IPASubDirName/Info.plist" | xml2 | grep -A 1 CFBundleDisplayName | tail -n1 | cut -d '=' -f 2`
 #echo $BUNDLE_TITLE
-BUNDLE_NAME=`$ParseInfo "tmp/Payload/$IPASubDirName.app/Info.plist" | xml2 | grep -A 1 CFBundleName | tail -n1 | cut -d '=' -f 2`
+BUNDLE_NAME=`$ParseInfo "tmp/Payload/$IPASubDirName/Info.plist" | xml2 | grep -A 1 CFBundleName | tail -n1 | cut -d '=' -f 2`
 #echo $BUNDLE_NAME
-MINIMUM_OSVERSION=`$ParseInfo "tmp/Payload/$IPASubDirName.app/Info.plist" | xml2 | grep -A 1 MinimumOSVersion | tail -n1 | cut -d '=' -f 2`
-ICON_FILENAME=`$ParseInfo "tmp/Payload/$IPASubDirName.app/Info.plist" | xml2 | grep -A 1 CFBundleIconFile | tail -n1 | cut -d '=' -f 2`
+MINIMUM_OSVERSION=`$ParseInfo "tmp/Payload/$IPASubDirName/Info.plist" | xml2 | grep -A 1 MinimumOSVersion | tail -n1 | cut -d '=' -f 2`
+ICON_FILENAME=`$ParseInfo "tmp/Payload/$IPASubDirName/Info.plist" | xml2 | grep -A 1 CFBundleIconFile | tail -n1 | cut -d '=' -f 2`
 
 [[ ${#BUNDLE_TITLE} == 0 ]] && BUNDLE_TITLE=$BUNDLE_NAME;
 
-unzip -q "$IPA" -d tmp/ "Payload/$IPASubDirName.app/$ICON_FILENAME*"
 ICON_PATH=`find tmp/Payload/ -name "$ICON_FILENAME*" | tail -n1`
 [[ ! -f $ICON_PATH ]] && echo "Pas d'icône dans l'application ? Nom : $ICON_FILENAME" && exit 1;
 
@@ -78,7 +79,7 @@ APPICON_URL="$RootURL/$RootDir/$RelativeDir/$ICON_FILENAME"
 
 #On poursuit en analysant le fichier embedded.mobileprovision pour avoir les dernières infos utiles (nom de l'équipe, les UUIDs pouvant installer l'IPA etc)
 #Pour analyser ce fichier (embedded.mobileprovision) il faut tout d'abord le nettoyer : l'entête et le pied de fichier sont en binaire et n'ont rien à voir avec le document XML constituant la PLIST qui nous intéresse.
-cat "tmp/Payload/$IPASubDirName.app/embedded.mobileprovision" | php -r '$C=file_get_contents("php://stdin"); $C=substr($C, strpos($C, "<?xml")); $C=substr($C, 0, strpos($C, "</plist>")+8); echo "$C\n";' > tmp/embedded.mobileprovision.plist
+cat "tmp/Payload/$IPASubDirName/embedded.mobileprovision" | php -r '$C=file_get_contents("php://stdin"); $C=substr($C, strpos($C, "<?xml")); $C=substr($C, 0, strpos($C, "</plist>")+8); echo "$C\n";' > tmp/embedded.mobileprovision.plist
 TMPFILENAME=tmp/embedded.mobileprovision.plist
 #APPIDNAME=`cat $TMPFILENAME | xml2 | grep -A 1 AppIDName | tail -n1 | cut -d '=' -f 2`
 APPIDNAME=$BUNDLE_NAME
